d191a2a4c0ef42c653c78a17663b73a8e0c6bab8
biffyw <biffyw@gmail.com>
2017-01-30 12:43:10 -0500

    update assignment

--- app.js
+++ app.js
@@ -7,10 +7,35 @@ angular.module('LunchCheck', [])
 LCController.$inject = ['$scope'];
 function LCController($scope) {
   $scope.lunchList = '';
-  $scope.message = 'hi mom';
+  $scope.message = '';
+  $scope.msgColour = 'black';
+
   $scope.listCheck = function () {
-    var n = $scope.lunchList.split(',');
-    console.log(n, n.length);
+    var list = $scope.lunchList.split(',');
+    var i, str, n = 0;
+    for (i in list) {
+      str = list[ i ].trim();
+      if (str.length > 0) {
+        n += 1;
+      }
+    }
+    console.log( 'items =', n)
+    if (n == 0) {
+      $scope.message = 'Please enter data first';
+    }
+    else if (n > 3) {
+      $scope.message = 'Too Much!';
+      $scope.msgColour = 'red';
+    }
+    else {
+      $scope.message = 'Enjoy!';
+      $scope.msgColour = 'green';
+    }
+
+    $scope.clearMessage = function() {
+      $scope.message = '';
+      $scope.msgColour = 'black';
+    }
   }
 }
 
