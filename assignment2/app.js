61486f3bb695d4bbc3e4c4c805ee55a7939ce2cf
biffyw <biffyw@gmail.com>
2017-02-10 11:58:05 -0500

    assignment 2

---
+++ assignment2/app.js
@@ -0,0 +1,48 @@
+(function() {
+  'use strict';
+
+  angular.module('ShoppingListCheckoff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckoffService', ShoppingListCheckoffService);
+
+  ToBuyController.$inject = ['ShoppingListCheckoffService'];
+  function ToBuyController( ShoppingListCheckoffService ) {
+    var need = this;
+
+    need.items = ShoppingListCheckoffService.getItemsToBuy();
+
+    need.boughtItem = function(index) {
+      ShoppingListCheckoffService.checkOffItem(index);
+    };
+  };
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckoffService'];
+  function AlreadyBoughtController( ShoppingListCheckoffService ) {
+    var have = this;
+
+    have.items = ShoppingListCheckoffService.getItemsAlreadyBought();
+  };
+
+  function ShoppingListCheckoffService() {
+      var service = this;
+
+      var itemsToBuy = ['10 foo', '29 bar', '8 baz'];
+      var itemsAlreadyBought = [];
+
+      service.getItemsToBuy = function() {
+        return itemsToBuy;
+      };
+
+      service.getItemsAlreadyBought = function() {
+        return itemsAlreadyBought;
+      };
+
+      service.checkOffItem = function( index ) {
+        var item = itemsToBuy[ index ];
+        itemsAlreadyBought.push(item);
+        itemsToBuy.splice(index, 1);
+      }
+  };
+
+})();
