997801101a22945bde2bb963b145efc44c13d22a
biffyw <biffyw@gmail.com>
2017-03-20 15:15:15 -0400

    more progress

--- assignment3/app.js
+++ assignment3/app.js
@@ -5,52 +5,67 @@ angular.module("NarrowItDownApp", [])
 .controller("NarrowItDownController", NarrowItDownController)
 .service("MenuSearchService", MenuSearchService)
 .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
-.directive('foundItems', FoundItems);
+.directive('foundItems', FoundItemsDirective);
 
-function FoundItems() {
-  var ddo = {
-    templateUrl: 'foundItems.html',
-    scope: {
-      found: '<',
-      onRemove: '@'
-    },
-    controller: NarrowItDownController,
-    controllerAs: 'narrow',
-    bindToController: true
-  };
-  return ddo;
-}
+function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        found: '<',
+        onRemove: '&'
+      },
+      controller: FoundItemsDirectiveController,
+      controllerAs: 'shortList',
+      bindToController: true
+    };
+    return ddo;
+};
+
+function FoundItemsDirectiveController() {
+    var shortList = this;
+
+    shortList.emptySearch = function() {
+      console.log('foo! foo!', shortList.found);
+      return true;
+    };
+};
 
 NarrowItDownController.$inject = ['MenuSearchService'];
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+
 function NarrowItDownController( MenuSearchService ) {
-  var narrow = this;
-  var found = [];
+  var shortList = this;
+  var foo = 'bar';
 
-  var searchTerm = 'fooo';
+  shortList.searchTerm = '';
 
-  // get a list of menu items that match
-  narrow.narrowClick = function() {
+  console.log("foo!");
+  shortList.narrowIt = function(searchTerm) {
+    if( shortList.searchTerm.trim().length === 0 ) {
+      shortList.found = [];
+      return;
+    };
+
     var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-
-    // display any results from the request
     promise.then(function (response) {
-      console.log("2 Got a response", response)
-      narrow.found = response;
-    })
-    .catch(function (error) {
-      console.log("2 Something went terribly wrong.");
-      narrow.found = [];
+      shortList.found = response;
+      for (var i in shortList.found) {
+        console.log(shortList.found[i].description.toLowerCase());
+      }
     });
   };
 
   // remove an item from the list
-  narrow.removeItem = function(index) {
-    console.log('narrow.removeItem:', index);
-    found.splice( index, 1);
+  shortList.removeItem = function(index) {
+    console.log('removeItem:', index);
+    shortList.found.splice( index, 1);
+  };
+
+  function getMessage() {
+    return ("blahblabhahbhbhbhb");
   };
 };
 
-MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService( $http, ApiBasePath ) {
   var service = this;
 
@@ -58,22 +73,16 @@ function MenuSearchService( $http, ApiBasePath ) {
     var response = $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json")
-    })
-    .then(function (response) {
-      console.log("Got: ", response.data.menu_items);
+    }).then(function (response) {
       var items = response.data.menu_items;
       var matches = []
       for( var i in items ) {
         if (items[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0)
-          console.log(items[i].description.toLowerCase());
           matches.push(items[i])
       }
       // return items that contain 'searchTerm' in the description
-
       return matches;
     });
-
-    console.log('getMatchedMenuItems', response);
     return response;
   };
 };
